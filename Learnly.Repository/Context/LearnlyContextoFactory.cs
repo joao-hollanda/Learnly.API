66e0954c9bf165e7b2f83b41d7787328ea010154
Hollanda <joao.martinshollanda@gmail.com>
2026-02-14 03:42:13 -0300

    Arrumar Leak

--- Learnly.Repository/Context/LearnlyContextoFactory.cs
+++ Learnly.Repository/Context/LearnlyContextoFactory.cs
@@ -1,14 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 public class LearnlyContextoFactory : IDesignTimeDbContextFactory<LearnlyContexto>
 {
     public LearnlyContexto CreateDbContext(string[] args)
     {
-        var optionsBuilder = new DbContextOptionsBuilder<LearnlyContexto>();
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables()
+            .Build();
 
-        optionsBuilder.UseNpgsql("Host=dpg-d5ppnan5c7fs73bthqr0-a.oregon-postgres.render.com;Port=5432;Database=learnly_db;Username=learnly_db_user;Password=rZ77YlBq2j6UE4CRoAr6CP5ezF0BX6Bo;SSL Mode=Require;Trust Server Certificate=true"
-);
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<LearnlyContexto>();
+        optionsBuilder.UseNpgsql(connectionString);
 
         return new LearnlyContexto(optionsBuilder.Options);
     }
